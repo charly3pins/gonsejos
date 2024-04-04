256e53e0aa072c1bf13e4c31de5f7bde6c3e07a9
charly3pins <charlyfb90@gmail.com>
2024-04-04 09:30:03 +0200

    underscore import

---
+++ underscore-import/domain/domain.go
@@ -0,0 +1,7 @@
+package domain
+
+import "fmt"
+
+func init() {
+	fmt.Println("init domain")
+}
---
+++ underscore-import/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	_ "underscore/db"
+	_ "underscore/domain"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
