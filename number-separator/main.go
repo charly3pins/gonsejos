1d0b97fe51f55208d62a894e5dd0e3854488159a
charly3pins <charlyfb90@gmail.com>
2024-04-07 22:47:00 +0200

    number separator

---
+++ number-separator/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const million = 1_000_000
+const randomFloat = 3.1231_12345123_1364643_136416436413_13646143634_134661431643_13264614364131463
+
+func main() {
+	fmt.Println(million)
+	fmt.Println(randomFloat)
+}
